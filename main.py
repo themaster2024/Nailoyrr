8c2bf98769e0b1ed938b82d867f36a201e07601b
themaster2024 <163314823+themaster2024@users.noreply.github.com>
2024-04-10 15:19:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,9 +35,9 @@ logging.basicConfig(
 
 bot = Client(
     "bot",
-    api_id= 22779671,
-    api_hash= "125d8d88b77309dc3b154cbbfc2dacb2",    
-    bot_token= "6847175705:AAHbkU8GFmzoxR9dCQTr6RuZ4NQsev5ufz0"
+    api_id= 29551974,
+    api_hash= "9bca2aa867336f6d010dd740d55f30c0",    
+    bot_token= "6878549524:AAHZOcfKraO5jD8f2amZjytyJynbiaUUWKc"
 )
 
 @bot.on_message(filters.command(["start"]))
